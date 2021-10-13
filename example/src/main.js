e709002a6efd93d643249a0e347aff7229b07432
Mr.YT <wuqishu@hodogame.com>
2021-10-13 10:25:08 +0800

    修改example组件引用

--- example/src/main.js
+++ example/src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 // 引入 ElFormRuleItem
-import ElFormRuleItem from '@/components/el-form-valid-item'
+import ElFormRuleItem from 'el-form-valid-item'
 import FormRules from './formRules'
 // 扩展校验规则
 ElFormRuleItem.extendRules(FormRules)
